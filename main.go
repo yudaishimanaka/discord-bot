45ec09ca3be72c5b6742f93a132d2ef5fb088d62
yudaishimanaka <gurutaminn.h8@gmail.com>
2018-04-08 03:18:56 +0900

    test bot

---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"github.com/bwmarrin/discordgo"
+	"log"
+	"strings"
+	"time"
+)
+
+var (
+	Token      = "Bot" + "YOUR_TOKEN" //"Bot"という接頭辞がないと401 unauthorizedエラーが起きます
+	BotName    = "<@YOUR_CLIENT_ID>"
+	stopBot    = make(chan bool)
+	HelloWorld = "!hello"
+)
+
+func main() {
+	//Discordのセッションを作成
+	discord, err := discordgo.New()
+	discord.Token = Token
+	if err != nil {
+		fmt.Println("Error logging in")
+		fmt.Println(err)
+	}
+
+	discord.AddHandler(onMessageCreate) //全てのWSAPIイベントが発生した時のイベントハンドラを追加
+	// websocketを開いてlistening開始
+	err = discord.Open()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println("Listening...")
+	<-stopBot //プログラムが終了しないようロック
+	return
+}
+
+func onMessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	c, err := s.State.Channel(m.ChannelID) //チャンネル取得
+	if err != nil {
+		log.Println("Error getting channel: ", err)
+		return
+	}
+	fmt.Printf("%20s %20s %20s > %s\n", m.ChannelID, time.Now().Format(time.Stamp), m.Author.Username, m.Content)
+
+	switch {
+	case strings.HasPrefix(m.Content, fmt.Sprintf("%s %s", BotName, HelloWorld)):
+		sendMessage(s, c, "私は桜ねね！！\nよろしく！！")
+	}
+}
+
+//メッセージを送信する関数
+func sendMessage(s *discordgo.Session, c *discordgo.Channel, msg string) {
+	_, err := s.ChannelMessageSend(c.ID, msg)
+
+	log.Println(">>> " + msg)
+	if err != nil {
+		log.Println("Error sending message: ", err)
+	}
+}
